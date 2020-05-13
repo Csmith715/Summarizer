5fad73402ab6b8d85753d17657d1812eb95296e7
Csmith715 <35317679+Csmith715@users.noreply.github.com>
2020-05-13 11:48:48 -0400

    Add files via upload

---
+++ extract_sections.py
@@ -0,0 +1,146 @@
+#### My Dash Code #####
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import dash_ui as dui
+
+import os
+import pickle
+
+from phrase_funcs import *
+
+external_stylesheets = ["https://codepen.io/rmarren1/pen/mLqGRg.css"]
+
+app = dash.Dash(__name__, external_stylesheets = external_stylesheets)
+#server = app.server
+
+
+with open('whatrules2.data', 'rb') as filehandle:
+    what_rules2 = pickle.load(filehandle)
+
+useable_text = '''This is where the useable text will go.
+
+There will be an introduction section of the page that will look like this. It will probably be one or two sentences.
+
+Then there might be a summary section that tells us about what we will learn in the webinar. This could be independent of a bulleted list that might follow.
+
+If there is a list, we would learn:
+- That a list isn't always necessary
+- They can tell us what we'll learn, but not in this case
+- There's no certainty that this list will be the one we need.
+
+'''
+
+grid = dui.Grid(_id="grid", num_rows=12, num_cols=12, grid_padding=1)
+
+# grid.add_element(
+#     col=1, row=1, width=10, height=2,
+#     element=html.Div([
+#         dcc.Upload(
+#             id='upload-data',
+#             children=html.Div([
+#                 'Drag and Drop or ',
+#                 html.A('Select Files')
+#             ]),
+#             style={
+#                 'width': '100%',
+#                 'height': '60px',
+#                 'lineHeight': '60px',
+#                 'borderWidth': '1px',
+#                 'borderStyle': 'dashed',
+#                 'borderRadius': '5px',
+#                 'textAlign': 'center',
+#                 'margin': '10px'
+#             })
+#         ])
+
+#     )
+
+grid.add_element(
+    col=1, row=1, width=4, height=3,
+    element=html.Div([
+        html.H1('Classifying Sections of Text'),
+        html.P('Paste text to be analyzed into the text box below.'),
+        html.Br(),
+        html.Div([dcc.Dropdown(
+            id='question-dropdown',
+            options=[
+                {'label': 'Broad', 'value': 'Broad'},
+                {'label': 'More Concise', 'value': 'More Concise'}
+            ],
+            clearable=False,
+            value='Broad',
+            style={'width': '60%', 'color': 'red', 'font-size': '110%'}
+        )
+            ])
+        ])
+    )
+grid.add_element(
+    col=1, row=5, width=4, height=7,
+    element=html.Div([dcc.Textarea(
+        id='input_body',
+        value=useable_text,
+        style={'width': '100%', 'height': '350px', 'color': 'blue', 'font-size': '130%'})
+        ])
+    )
+grid.add_element(
+    col=6, row=1, width=4, height=5,
+    element=html.Div([html.Br(),
+        html.H3('Bulleted Summary:'),
+        html.Div([dcc.Markdown(id='result1', style={'font-size': '130%'})]),
+        #html.Div(id='result1', style={'font-size': '120%'}),
+        html.Br()
+        ])
+    )
+grid.add_element(
+    col=6, row=7, width=4, height=5,
+    element=html.Div([html.Br(),
+        html.H3('Sentence Summary:'),
+        html.Div([dcc.Markdown(id='result2', style={'font-size': '130%'})]),
+        #html.Div(id='result2', style={'font-size': '120%'}),
+        html.Br()
+        ])
+    )
+
+app.layout = html.Div(grid.get_component(), style={
+    "height": "calc(100vh - 20px)",
+    "width": "calc(100vw - 20px)"
+})
+
+# def parse_contents(contents, filename, date):
+#     content_type, content_string = contents.split(',')
+
+#     decoded = base64.b64decode(content_string)
+#     try:
+#         if 'csv' in filename:
+#             # Assume that the user uploaded a CSV file
+#             df = pd.read_csv(
+#                 io.StringIO(decoded.decode('utf-8')))
+#         elif 'xls' in filename:
+#             # Assume that the user uploaded an excel file
+#             df = pd.read_excel(io.BytesIO(decoded))
+
+#     return df
+
+@app.callback(
+    [Output('result1','children'),
+     Output('result2','children')],
+    [Input('question-dropdown', 'value'),
+     Input('input_body', 'value')]
+)
+def find_sections(input_selection, input_body):
+    if input_selection == 'More Concise':
+        l, n = split_text_and_extract_sents(input_body)
+
+        return l, n
+
+    elif input_selection == 'Broad':
+        l, n = split_listed_text(input_body, what_rules2)
+
+        return l, n
+
+
+if __name__ == '__main__':
+    app.run_server(debug=False)
---
+++ phrase_funcs.py
@@ -0,0 +1,125 @@
+import pandas as pd
+import numpy as np
+import difflib
+from itertools import chain, groupby
+from operator import itemgetter
+
+import spacy
+from spacy.matcher import Matcher
+from spacy.matcher import PhraseMatcher
+import pickle
+
+with open('qterms.data', 'rb') as filehandle:
+    qterms = pickle.load(filehandle)
+
+nlp = spacy.load('en_core_web_sm')
+matcher = PhraseMatcher(nlp.vocab)
+terms = qterms
+patterns = [nlp.make_doc(text) for text in terms]
+matcher.add("TerminologyList", None, *patterns)
+
+def spacy_phrase_match(text):
+    doc = nlp(text)
+    matches = matcher(doc)
+    matching_sents = []
+    for match_id, start, end in matches:
+        sent_span = doc[start:end].sent
+        matching_sents.append(sent_span.text)
+    
+    return np.unique(matching_sents)
+
+def get_pattern_tupples(text):
+    doc = nlp(text)
+    pattern = [(t.pos_, t.tag_, t.dep_) for t in doc]
+    term_idx = [str(t) for t in doc]
+    
+    return pattern, term_idx
+def get_qphrases_text(rules, text):
+    fnd = []
+    pat, t = get_pattern_tupples(text)
+    for w in rules:
+        rm = rule_matches(pat, w)
+        if rm:
+            fnd.append(text)
+    
+    return(fnd)
+
+def rule_matches(a, b):
+    sm = difflib.SequenceMatcher(None, a, b).get_matching_blocks()
+    seq_match = [[s[0],s[0]+s[2]] for s in sm if s[2] == len(b)]
+    return(seq_match)
+
+def split_text_and_extract_sents(text):
+    ulist = text.split('\n')
+    ulist = [u for u in ulist if u]
+    list_idxs = []
+    for i, x in enumerate(ulist):
+        if x[:2] == '- ':
+            list_idxs.append(i)
+
+    list_groups = []
+    for k, g in groupby(enumerate(list_idxs), lambda x: x[0]-x[1]):
+         list_groups.append(list(map(itemgetter(1), g)))
+
+    webinar_list_text = []
+    new_list_idxs = []
+    for g in list_groups:
+        if g[0] != 0:
+            a = min(g)-1
+            list_text = '\n'.join(ulist[a:max(g)+1])
+            list_idxs.append(a)
+        else:
+            list_text = ''
+        webinar_list_text.append(list_text)
+        new_list_idxs.append(g)
+        
+    non_list_text = [x for i, x in enumerate(ulist) if i not in list_idxs]
+    
+    wlt = [spacy_phrase_match(s) for s in webinar_list_text]
+    wlt = pd.unique(list(chain.from_iterable(wlt)))
+
+    nlt = [spacy_phrase_match(n) for n in non_list_text]
+    nlt = pd.unique(list(chain.from_iterable(nlt)))
+    nlt = '\n'.join(nlt)
+    
+    return wlt, nlt
+
+def split_listed_text(text, rules):
+    ulist = text.split('\n')
+    ulist = [u for u in ulist if u]
+    list_idxs = []
+    for i, x in enumerate(ulist):
+        if x[:2] == '- ':
+            list_idxs.append(i)
+    
+    list_groups = []
+    for k, g in groupby(enumerate(list_idxs), lambda x: x[0]-x[1]):
+         list_groups.append(list(map(itemgetter(1), g)))
+
+    webinar_list_text = []
+    new_list_idxs = []
+    for g in list_groups:
+        if g[0] != 0:
+            a = min(g)-1
+            list_text = '\n'.join(ulist[a:max(g)+1])
+            list_idxs.append(a)
+        else:
+            list_text = ''
+        webinar_list_text.append(list_text)
+        new_list_idxs.append(g)
+
+    if new_list_idxs and (min(chain.from_iterable(new_list_idxs)) != 0):
+        min_list_idx = min(chain.from_iterable(new_list_idxs))
+    else:
+        min_list_idx = len(ulist)
+ 
+    non_list_text = [x for i, x in enumerate(ulist) if i not in list_idxs and i < min_list_idx]
+    
+    wlt = [get_qphrases_text(rules, s) for s in webinar_list_text]        
+    nlt = [get_qphrases_text(rules, n) for n in non_list_text]
+    nlt = pd.unique(list(chain.from_iterable(nlt)))
+    nlt = '\n'.join(nlt)
+    
+    wlt = pd.unique(list(chain.from_iterable(wlt)))
+    
+    return wlt, nlt
