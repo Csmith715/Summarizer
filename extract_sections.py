3beaf0554b9a484965b9b6ff543b545c3975baca
Csmith715 <35317679+Csmith715@users.noreply.github.com>
2020-05-20 11:22:14 -0400

    Update extract_sections.py

--- extract_sections.py
+++ extract_sections.py
@@ -17,9 +17,6 @@ app = dash.Dash(__name__, external_stylesheets = external_stylesheets)
 server = app.server
 
 
-with open('whatrules2.data', 'rb') as filehandle:
-    what_rules2 = pickle.load(filehandle)
-
 useable_text = '''This is where the useable text will go.
 
 There will be an introduction section of the page that will look like this. It will probably be one or two sentences.
@@ -57,89 +54,76 @@ grid = dui.Grid(_id="grid", num_rows=12, num_cols=12, grid_padding=1)
 #         ])
 
 #     )
-
+####### Left side of the page
 grid.add_element(
-    col=1, row=1, width=4, height=3,
+    col=1, row=1, width=4, height=1,
     element=html.Div([
         html.H1('Classifying Sections of Text'),
         html.P('Paste text to be analyzed into the text box below.'),
-        html.Br(),
-        html.Div([dcc.Dropdown(
-            id='question-dropdown',
-            options=[
-                {'label': 'Broad', 'value': 'Broad'},
-                {'label': 'More Concise', 'value': 'More Concise'}
-            ],
-            clearable=False,
-            value='Broad',
-            style={'width': '60%', 'color': 'red', 'font-size': '110%'}
-        )
-            ])
         ])
     )
 grid.add_element(
-    col=1, row=5, width=4, height=7,
+    col=1, row=2, width=4, height=7,
     element=html.Div([dcc.Textarea(
         id='input_body',
         value=useable_text,
         style={'width': '100%', 'height': '350px', 'color': 'blue', 'font-size': '130%'})
         ])
     )
-grid.add_element(
-    col=6, row=1, width=4, height=5,
-    element=html.Div([html.Br(),
-        html.H3('Bulleted Summary:'),
-        html.Div([dcc.Markdown(id='result1', style={'font-size': '130%'})]),
-        #html.Div(id='result1', style={'font-size': '120%'}),
-        html.Br()
-        ])
-    )
-grid.add_element(
-    col=6, row=7, width=4, height=5,
-    element=html.Div([html.Br(),
-        html.H3('Sentence Summary:'),
-        html.Div([dcc.Markdown(id='result2', style={'font-size': '130%'})]),
-        #html.Div(id='result2', style={'font-size': '120%'}),
-        html.Br()
-        ])
-    )
 
+####### Right side of the page
+grid.add_element(
+    col=6, row=1, width=4, height=1,
+    element=html.Div([html.H3('Introduction:')]))
+grid.add_element(
+    col=6, row=2, width=4, height=2,
+    element=html.Div([
+        html.Div([dcc.Markdown(id='result1', style={'font-size': '130%', 'border-style': 'solid', 'padding': '1px', 'border-width': '2px'})])
+        ])
+    )
+grid.add_element(
+    col=6, row=4, width=4, height=1,
+    element=html.Div([html.H3('Sentence "What you will learn":')]))
+grid.add_element(
+    col=6, row=5, width=4, height=2,
+    element=html.Div([
+        html.Div([dcc.Markdown(id='result2', style={'font-size': '130%', 'border-style': 'solid', 'padding': '1px', 'border-width': '2px'})])
+        ])
+    )
+grid.add_element(
+    col=6, row=7, width=4, height=1,
+    element=html.Div([html.H3('List "What you will learn":')]))
+grid.add_element(
+    col=6, row=8, width=4, height=2,
+    element=html.Div([
+        html.Div([dcc.Markdown(id='result3', style={'font-size': '130%', 'border-style': 'solid', 'padding': '1px', 'border-width': '2px'})])
+        ])
+    )
+grid.add_element(
+    col=6, row=10, width=4, height=1,
+    element=html.Div([html.H3('Speaker(s):')]))
+grid.add_element(
+    col=6, row=11, width=4, height=2,
+    element=html.Div([
+        html.Div([dcc.Markdown(id='result4', style={'font-size': '130%', 'border-style': 'solid', 'padding': '1px', 'border-width': '2px'})])
+        ])
+    )
 app.layout = html.Div(grid.get_component(), style={
     "height": "calc(100vh - 20px)",
     "width": "calc(100vw - 20px)"
 })
 
-# def parse_contents(contents, filename, date):
-#     content_type, content_string = contents.split(',')
-
-#     decoded = base64.b64decode(content_string)
-#     try:
-#         if 'csv' in filename:
-#             # Assume that the user uploaded a CSV file
-#             df = pd.read_csv(
-#                 io.StringIO(decoded.decode('utf-8')))
-#         elif 'xls' in filename:
-#             # Assume that the user uploaded an excel file
-#             df = pd.read_excel(io.BytesIO(decoded))
-
-#     return df
-
 @app.callback(
     [Output('result1','children'),
-     Output('result2','children')],
-    [Input('question-dropdown', 'value'),
-     Input('input_body', 'value')]
+     Output('result2','children'),
+     Output('result3','children'),
+     Output('result4','children')],
+    [Input('input_body', 'value')]
 )
-def find_sections(input_selection, input_body):
-    if input_selection == 'More Concise':
-        l, n = split_text_and_extract_sents(input_body)
+def find_sections(input_body):
+    i, s, l, sp = extract_sections(input_body)
 
-        return l, n
-
-    elif input_selection == 'Broad':
-        l, n = split_listed_text(input_body, what_rules2)
-
-        return l, n
+    return i, s, l, sp
 
 
 if __name__ == '__main__':
